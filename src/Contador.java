fd6920faaafb485ad56d508079efeb5190b3be33
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-05-28 16:25:33 -0300

    Adição de 'nextDouble' nos dois parâmetros

--- src/Contador.java
+++ src/Contador.java
@@ -3,9 +3,9 @@ public class Contador {
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextLine();
+		int parametroUm = terminal.nextDouble();
 		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextLine();
+		int parametroDois = terminal.nextDouble();
 		
 		try {
 			//chamando o método contendo a lógica de contagem
