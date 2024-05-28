82eb16651c0a000ad9037c5feb3924b5819f0688
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-05-28 17:09:36 -0300

    Extendendo a classe parametrosInvalidosException como exceção e finalizando o if else

--- src/Contador.java
+++ src/Contador.java
@@ -1,25 +1,32 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class Contador {
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextDouble();
+		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextDouble();
+		int parametroDois = terminal.nextInt();
 		
 		try {
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}catch (? exception) {
+		}catch (ParametrosInvalidosException exception) {
 			System.out.println("O segundo parâmetro deve ser maior que o primeiro");//imprimir a mensagem: 
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		
-		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
+		if (parametroUm > parametroDois) {
+			throw ParametrosInvalidosException();
+		} else {
+
+			int contagem = parametroDois - parametroUm;
+			//realizar o for para imprimir os números com base na variável contagem
+			for(int i = 0; i < contagem; i++){
+				System.out.println("Imprimindo o número " + (i+1));
+			}
+		}
 	}
 }
