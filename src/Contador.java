1ea5a21e2fd74ca1f7c67ec787d6312b435c1768
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-05-28 16:23:54 -0300

    Adição de 'nextLine' nos dois parâmetros

--- src/Contador.java
+++ src/Contador.java
@@ -3,9 +3,9 @@ public class Contador {
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.??;
+		int parametroUm = terminal.nextLine();
 		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
+		int parametroDois = terminal.nextLine();
 		
 		try {
 			//chamando o método contendo a lógica de contagem
